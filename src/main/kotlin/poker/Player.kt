0e0fa88c1357739f8643cf1143bf5c1ce3d79925
Bence Kiglics <bence.kiglics@prezi.com>
2015-04-20 18:45:17 +0200

    raising with the min_raise

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -5,16 +5,22 @@ import org.json.JSONObject
 
 class Player {
     fun betRequest(game_state: JSONObject): Int {
-//        for (player in 1..(game_state.get("players") as JSONArray).length()) {
-//            player.
-//        }
-        return 1000
+        val player = currentPlayer(game_state)
+        val min_raise = (game_state.get("current_buy_in") as Int) - (player.get("bet") as Int)
+        val cards = getPlayerCards(player)
+
+        return min_raise
     }
+
+    private fun getPlayerCards(player: JSONObject) = player.get("hole_cards") as JSONArray
+
+    private fun currentPlayer(game_state: JSONObject) =
+            (game_state.get("players") as JSONArray)[game_state.get("in_action") as Int] as JSONObject
 
     fun showdown() {
     }
 
     fun version(): String {
-        return "Kotlin Player 0.0.1"
+        return "Bnc's Kotlin Player 0.0.2"
     }
 }
