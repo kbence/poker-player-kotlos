5786167901661e4f9e58f8d0b3d4e4879ce1f852
Bence Kiglics <bence.kiglics@prezi.com>
2015-04-20 18:56:00 +0200

    Trick against DeVill

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -9,7 +9,14 @@ class Player {
         val min_raise = (game_state.get("current_buy_in") as Int) - (player.get("bet") as Int)
         val cards = getPlayerCards(player)
 
-        return min_raise*2
+        val cr1 = (cards[0] as JSONObject).get("rank") as String
+        val cr2 = (cards[1] as JSONObject).get("rank") as String
+
+        return if (cr1 == cr2) {
+            min_raise*2
+        } else {
+            0
+        }
     }
 
     private fun getPlayerCards(player: JSONObject) = player.get("hole_cards") as JSONArray
