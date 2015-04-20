3ac8083769d3cf2404caf067a6edfdfc14c5aadb
Bence Kiglics <bence.kiglics@prezi.com>
2015-04-20 18:32:53 +0200

    deploy noowowwwwwewoww!!!!!!

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -1,9 +1,13 @@
 package poker
 
+import org.json.JSONArray
 import org.json.JSONObject
 
 class Player {
     fun betRequest(game_state: JSONObject): Int {
+//        for (player in 1..(game_state.get("players") as JSONArray).length()) {
+//            player.
+//        }
         return 1000
     }
 
